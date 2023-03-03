ac77086074b1a48c1439b698e0f426a3faaa6b7d
Derek Schwantner <derek@Dereks-Mac-mini.local>
2023-03-03 01:02:37 -0500

    added tailwind dependancy and tailwind config

--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -1,11 +1,37 @@
 import { gql, ApolloServer } from 'apollo-server-micro'
+import { PrismaClient } from '@prisma/client';
 
-const typeDefs = gql``
+const prisma = new PrismaClient();
 
-const resolvers = {}
+// define the types that we will be working with
+const typeDefs = gql`
+  type Post {
+    id: String
+    text: String
+  }
+
+  type Query {
+    posts: [Post]
+  }
+
+  type Mutation {
+    createPost(text: String): Post
+  }`;
+
+// define the resolvers that we will be working with, scheme needs to match the typeDefs
+const resolvers = {
+    Query: {
+        posts: (_parent, _args, _context) => {
+            return prisma.post.findMany();
+        }
+    }
+}
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
+
 const handler = apolloServer.createHandler({ path: '/api/graphql' });
+
+export const config = { api: { bodyParser: false } };
 
 export default handler;
